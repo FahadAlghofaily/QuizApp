fc2342bebd6a6227a38a8a7e55df536d5897e1a7
Fahad Alghofaily <alghofaily.fahad@gmail.com>
2017-07-29 19:35:04 +0300

    Logical Modification on calculating the score

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int score = 0 ;
@@ -17,33 +18,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    protected void check(View view){
+    public void check(View view){
         RadioButton radioButton = (RadioButton) findViewById(R.id.right_radio);
         CheckBox checkBox1 = (CheckBox) findViewById(R.id.check1);
         CheckBox checkBox2 = (CheckBox) findViewById(R.id.check2);
         CheckBox checkBox3 = (CheckBox) findViewById(R.id.check3);
+        CheckBox checkBox4 = (CheckBox) findViewById(R.id.check4);
         EditText editText1 = (EditText) findViewById(R.id.edit_text_1);
         EditText editText2 = (EditText) findViewById(R.id.edit_text_2);
         final TextView scoreView = (TextView) findViewById(R.id.score_text);
+        if (checkBox1.isChecked() && checkBox2.isChecked() && checkBox3.isChecked() && !checkBox4.isChecked()) {
+            score += 15;
+        } else if(checkBox1.isChecked() && checkBox2.isChecked() && !checkBox3.isChecked() && !checkBox4.isChecked() ||
+                checkBox1.isChecked() && !checkBox2.isChecked() && checkBox3.isChecked() && !checkBox4.isChecked() ||
+                !checkBox1.isChecked() && checkBox2.isChecked() && checkBox3.isChecked() && !checkBox4.isChecked() ){
+            score += 10;
+        } else if (checkBox1.isChecked() && !checkBox2.isChecked() && !checkBox3.isChecked() && !checkBox4.isChecked() ||
+                !checkBox1.isChecked() && !checkBox2.isChecked() && checkBox3.isChecked() && !checkBox4.isChecked() ||
+                !checkBox1.isChecked() && checkBox2.isChecked() && !checkBox3.isChecked() && !checkBox4.isChecked() ){
+            score += 5 ;
+        } else {
+            score += 0 ;
+        }
         if(radioButton.isChecked()){
-            score = score + 5 ;
+            score = score + 10 ;
         }
-        if(checkBox1.isChecked()){
-            score = score + 5 ;
-        }
-        if(checkBox2.isChecked()){
-            score = score + 5 ;
-        }
-        if(checkBox3.isChecked()){
-            score = score + 5 ;
-        }
+
         if(editText1.getText().toString().equalsIgnoreCase("asia")){
             score = score + 5 ;
         }
         if(editText2.getText().toString().equalsIgnoreCase("16")){
             score = score + 5 ;
         }
-        scoreView.setText("Your score= " + score + "/30");
+
+        Toast.makeText(this, "Your score= " + score + "/35", Toast.LENGTH_SHORT).show();
         score = 0;
     }
 }
